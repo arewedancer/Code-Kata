dbcc57bdff3d5cdadec383ea5d27c6ff29646fdb
arewedancer <arewedancer111@gmail.com>
2018-10-04 10:06:02 -0500

    no-lookahed strategy

--- 2048.cc
+++ 2048.cc
@@ -1,4 +1,31 @@
 #include <iostream>
+template <class T>
+void deep_copy(T** dest, T** src)
+{};
+template <class T>
+double get_score( T** b)
+{
+};
+int findBestMove()
+{
+	//choice [up, down, left, right]= (0,1,2,3)
+	// score = sum(total number value of board) /sum (total number of non-zero blocks)
+	double score[4];
+	int b_copy[row][col];
+	int * bc_copy[row][col];
+	deep_copy(board, b_copy);
+	slide_up(b_copy);
+	score[0] = get_score(b_copy);
+	deep_copy(board, b_copy);
+	slide_down(b_copy);
+	score[1] = get_score(b_copy);
+	deep_copy(boardByCol, bc_copy);
+	slide_left(bc_copy);
+	score[2] = get_score(bc_copy);
+	deep_copy(boardByCol, bc_copy);
+	slide_right(bc_copy);
+	score[3] = get_score(bc_copy);
+};
 
 void slide(int start, int end, int step, int *b )
 {
@@ -51,11 +78,11 @@ void slide_right()
 
 const int row = 8;
 const int col = 8;
+int board[row][col];	
+int* boardBycol[col][row];
 int main()
 {
-	int board[row][col];	
-	int* boardBycol[col][row];
-  for(int i = 0; i < col; i++ )
-  	for(int j = 0; j < row; i++ )
-		boardBycol[i][j] = &board[j][i];
+	for(int i = 0; i < col; i++ )
+		for(int j = 0; j < row; i++ )
+			boardBycol[i][j] = &board[j][i];
 };
